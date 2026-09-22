bc5000fc11d05e1d8df62851dca9383c735ccf05
pichiefox <citrazad@gmail.com>
2026-09-22 10:23:28 +0700

    Soal 1 : a PYTHON program to evaluate the student performance

---
+++ soal1.py
@@ -0,0 +1,9 @@
+Performance = int(input ("Enter Student Performance : "))
+if Performance >= 90:
+    print ("Excellent Performance")
+elif Performance >= 80:
+    print ("Very Good Performance")
+elif Performance >= 70:
+    print ("Good Performance")
+else : 
+    print ("Average Performance")
